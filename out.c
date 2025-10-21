2ed3ccca057fbf905c42e03da5dae37754f5e5dc
TheZetmix <proenicso2@gmail.com>
2025-10-21 07:44:23 +0000

    another test

--- out.c
+++ out.c
@@ -1,6 +1,2 @@
 # include "stdio.h" 
- 
- int sum ( int a , int b ) { 
- return a + b ; 
- } 
-  typedef struct { int a ; int b ; } Some ; int main ( ) { int test ; }
+ typedef struct { int x ; int y ; } Vec2 ; int main ( ) { }
