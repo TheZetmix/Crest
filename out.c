fa9f03ba083571e9e76af605a0fa9388e2ff7867
TheZetmix <proenicso2@gmail.com>
2025-10-21 07:56:37 +0000

    test

--- out.c
+++ out.c
@@ -1,2 +1,2 @@
 # include "stdio.h" 
- typedef struct { int x ; int y ; } Vec2 ; int main ( ) { }
+ typedef struct { int x ; int y ; } Vec2 ; int Vec2_sum ( Vec2* this ) { return this -> x + this -> y ; } int main ( ) { Vec2 some_vec = { 10 , 15 } ; }
